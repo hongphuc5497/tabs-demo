2a685a212639b37475ce24bb28675d49e1d7fe9b
Hong Phuc <og.viet.003.dev@gmail.com>
2020-01-20 15:34:18 +0700

    tab-list

---
+++ src/components/Tabs.js
@@ -0,0 +1,55 @@
+// @ts-nocheck
+import React, * as react from 'react';
+import PropTypes from 'prop-types';
+import Tab from './Tab';
+
+class Tabs extends react.Component {
+  static propTypes = {
+    children: PropTypes.instanceOf(Array).isRequired
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTab: this.props.children[0].props.label
+    };
+  }
+
+  onClickTabItem = (tab) => this.setState({ activeTab: tab })
+
+  render() {
+    const {
+      onClickTabItem,
+      props: {
+        children
+      },
+      state: {
+        activeTab
+      }
+    } = this;
+  }
+
+  return (
+    <div className="tabs">
+      <ol className="tab-list">
+        {children.map((child) => {
+          const { label } = child.props;
+
+          return (
+            <Tab
+              activeTab={activeTab}
+              key={label}
+              label={label}
+              onClick={this.onClickTabItem}
+            />
+          );
+        })}
+      </ol>
+      
+      <div className="tab-content"></div>
+    </div>
+  )
+}
+
+export default Tabs;
